0d95ff472c43f3d868081e0904e7ec1137b5fb1b
Frederic Beckmann <f.beckmann@eas-solutions.de>
2022-12-16 11:26:54 +0100

    Laden der Projekte hinzugefügt

--- Powershell/ImportAndLogin.ps1
+++ Powershell/ImportAndLogin.ps1
@@ -2,7 +2,7 @@
 Add-Type -Path "$PSScriptRoot\..\bin\EAS.LeegooBuilder.Web.WebApiClient.dll" -IgnoreWarnings
 
 # API Client erzeugen
-$uri = New-Object System.Uri -arg "http://avalon.eas-cpq.de:56540/api/"
+$uri = New-Object System.Uri -arg "http://localhost:56540/api/"
 $apiClient = New-Object EAS.LeegooBuilder.Web.WebApiClient.WebApiClient -arg $uri
 
 Write-Output "Client loaded"
--- Powershell/Projects/200.LoadProjects.ps1
+++ Powershell/Projects/200.LoadProjects.ps1
@@ -2,14 +2,22 @@
 $apiClient = & "$PSScriptRoot\..\ImportAndLogin.ps1"
 
 # Abfrage
+$content = New-Object Collections.Generic.List[EAS.LeegooBuilder.Web.Contracts.Models.ParameterClasses.ProjectServiceWeb.GetProjectsContent]
+$content.Add([EAS.LeegooBuilder.Web.Contracts.Models.ParameterClasses.ProjectServiceWeb.GetProjectsContent]::All)
+
+$loadOptions = New-Object Collections.Generic.List[EAS.LeegooBuilder.Web.Contracts.Models.Enums.ProjectLoadType]
+$loadOptions.Add([EAS.LeegooBuilder.Web.Contracts.Models.Enums.ProjectLoadType]::AllProjects)
+
 $paramter = New-Object EAS.LeegooBuilder.Web.Contracts.Models.ParameterClasses.ProjectServiceWeb.GetProjectsParameter
+$paramter.LoadOptions = $loadOptions
+$paramter.ProjectsContent = $content
 
 $result = $apiClient.ProjectService.GetProjects($paramter)
 
 # Ausgabe
 if ($result.OperationResult.Successful) {
     foreach ($project in $result.Projects) {
-        Write-Output "$($project.Description) - $($culture.ShortText)"
+        Write-Output "$($project.ProjectID) - $($project.Description) - $($project.Note)"
     }
 } else {
     Write-Output $result.OperationResult.ShortMessage
