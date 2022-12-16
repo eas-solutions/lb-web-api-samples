97c12d6fe7cd6d8104255bda79ad06af165aa5cc
Frederic Beckmann <mixmix99@hotmail.de>
2022-12-16 14:28:00 +0100

    Readme erweitert und Funktionen etwas umstrukturiert

--- Powershell/ImportAndLogin.ps1
+++ Powershell/ImportAndLogin.ps1
@@ -1,16 +1,23 @@
 # Load Client DLL
 Add-Type -Path "$PSScriptRoot\..\bin\EAS.LeegooBuilder.Web.WebApiClient.dll" -IgnoreWarnings
 
+# !!! Change Here !!!
+$apiUrl = "http://avalon.eas-cpq.de:56540/api/"
+$username = "Administrator"
+$password = "admin"
+$culture = "de-DE"
+$language = "de"
+
 # Create Client Instance
-$uri = New-Object System.Uri -arg "http://localhost:56540/api/"
+$uri = New-Object System.Uri -arg $apiUrl
 $apiClient = New-Object EAS.LeegooBuilder.Web.WebApiClient.WebApiClient -arg $uri
 
 # Create Login Parameter
 $loginParameter = New-Object EAS.LeegooBuilder.Web.Contracts.Models.ParameterClasses.UserServiceWeb.LoginParameter
-$loginParameter.Username = "Administrator"
-$loginParameter.Password = "admin"
-$loginParameter.Culture = "de-DE"
-$loginParameter.Language = "de"
+$loginParameter.Username = $username
+$loginParameter.Password = $password
+$loginParameter.Culture = $culture
+$loginParameter.Language = $language
 
 # Execute Login
 $loginResult = $apiClient.LogIn($loginParameter)
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # LeegooBuilderWeb-API-Samples
 
-Eine Sammlung von Beispielen für die Nutzung der 
-LeegooBuilderWeb API in verschiedenen Sprachen
+This repository contains several excamples for the communication with the LEEGOO BUILDER G3 Web API.
+
+
+# Powershell
+
+## Recommendation
+The easies way to try out the powershell excamples is [Visual Studio Code](https://code.visualstudio.com/). Here you can start the scripts easily, debug them and additionaly there is some intellisense.
+
+## Preperations
+The powershell examples are designed to look at the folder `bin` in the repository root for the LEEGOO BUILDER Api Client DLL´s. So you have to copy the client with all the dependencies in this folder before trying out the samples.
+
+## Structure
+
+### ImportAndLogin.ps1
+This script loads the client Dll´s and connects to the api. Additionaly you need to specify the access data in this file. 
+
